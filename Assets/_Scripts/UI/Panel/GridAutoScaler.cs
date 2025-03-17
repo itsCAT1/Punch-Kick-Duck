ad0f40fe1024e10e28eee919ea4cf308dc1ad0e5
itsCAT1 <anhhtu199@gmail.com>
2025-03-17 17:43:14 +0700

    Screen

---
+++ Assets/_Scripts/UI/Panel/GridAutoScaler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GridAutoScaler : MonoBehaviour
+{
+    public GridLayoutGroup gridLayout;
+
+    private void OnValidate()
+    {
+        gridLayout = GetComponent<GridLayoutGroup>();
+    }
+
+    private void OnEnable()
+    {
+        AdjustCellSize();
+    }
+
+    void AdjustCellSize()
+    {
+        // Lấy kích thước màn hình
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+
+        // Chia 3 cột, 2 hàng
+        float cellHeight = screenHeight / 3;
+        float cellWidth = screenHeight * 2.1f;
+
+        // Đặt Cell Size
+        gridLayout.cellSize = new Vector2(cellWidth, cellHeight);
+
+        Debug.Log(screenWidth);
+        Debug.Log(screenHeight);
+    }
+}
